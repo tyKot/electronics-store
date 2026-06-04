5d13ae4b046726db21f7161771f53b8fd8972f63
tyKot <overk456@gmail.com>
2026-06-04 20:46:42 +0400

    last race

--- routes/api.php
+++ routes/api.php
@@ -3,11 +3,11 @@
 use App\Http\Controllers\Api\BotController;
 // ,'throttle:bot-api'
 Route::prefix('bot')->middleware(['bot.token'])->group(function () {
-    Route::get('/order-status', [BotController::class, 'getOrderStatus']);
+    Route::post('/order-status', [BotController::class, 'getOrderStatus']);
     Route::post('/search-products', [BotController::class, 'searchProducts']);
     Route::post('/get-product-details', [BotController::class, 'getProductDetails']);
     Route::post('/get-popular-categories', [BotController::class, 'getPopularCategories']);
 });
 
 
-Route::get('/bot/debug', [BotController::class, 'debug']);
+Route::post('/bot/debug', [BotController::class, 'debug']);
